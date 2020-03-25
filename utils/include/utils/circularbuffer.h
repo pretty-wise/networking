24fe3ec46f90ef037d6b1dbc5f6300af02d37d51
prettywise <krzysiek.stasik@gmail.com>
2020-03-25 12:21:55 -0400

    * change circular buffer implementation to work for element counts lower than page size

--- utils/include/utils/circularbuffer.h
+++ utils/include/utils/circularbuffer.h
@@ -2,13 +2,7 @@
 #include <assert.h>
 #include <stdint.h>
 
-class CircularBufferBase {
-protected:
-  void *Allocate(size_t *size);
-  void Deallocate(void *buffer, size_t size);
-};
-
-template <class T> class CircularBuffer : protected CircularBufferBase {
+template <class T> class CircularBuffer {
 public:
   CircularBuffer(size_t num_elements);
   ~CircularBuffer();
@@ -28,7 +22,9 @@ public:
   void Clear();
 
 private:
-  size_t m_buffer_size;
+  CircularBuffer(const CircularBuffer &);
+  CircularBuffer &operator=(const CircularBuffer &);
+
   T *m_buffer;
   T *m_read;
   T *m_write;
@@ -36,33 +32,26 @@ private:
 };
 
 template <class T> CircularBuffer<T>::CircularBuffer(size_t num_elements) {
-  num_elements += 1; // for last-first spacing
-
-  m_buffer_size = sizeof(T) * num_elements;
-  m_buffer = (T *)Allocate(&m_buffer_size);
-  assert(m_buffer_size % sizeof(T) == 0);
-
-  m_capacity = m_buffer_size / sizeof(T);
-
+  m_buffer = new T[2 * num_elements];
+  m_capacity = num_elements;
   m_read = m_buffer;
   m_write = m_buffer;
 }
 
-template <class T> CircularBuffer<T>::~CircularBuffer() {
-  Deallocate(m_buffer, m_buffer_size);
-}
+template <class T> CircularBuffer<T>::~CircularBuffer() { delete[] m_buffer; }
 
 template <class T> size_t CircularBuffer<T>::PushBack(size_t count) {
   if(count > Capacity()) {
     count = Capacity();
   }
 
-  size_t elements_left = m_capacity - Size() - 1;
+  size_t elements_left = m_capacity - Size();
   if(count > elements_left) {
     PopFront(count - elements_left);
   }
 
   m_write += count;
+  assert(m_write < m_buffer + 2 * m_capacity);
   return count;
 }
 template <class T> size_t CircularBuffer<T>::PopFront(size_t count) {
@@ -75,6 +64,10 @@ template <class T> size_t CircularBuffer<T>::PopFront(size_t count) {
     m_read -= m_capacity;
     m_write -= m_capacity;
   }
+  assert(m_read >= m_buffer);
+  assert(m_write >= m_buffer);
+  assert(m_write < m_buffer + (2 * m_capacity));
+  assert(m_read < m_buffer + (2 * m_capacity));
   return count;
 }
 
@@ -87,7 +80,7 @@ template <class T> size_t CircularBuffer<T>::Size() const {
   return m_write - m_read;
 }
 template <class T> size_t CircularBuffer<T>::Capacity() const {
-  return m_capacity - 1;
+  return m_capacity;
 }
 
 template <class T> void CircularBuffer<T>::Resize(size_t size) {
@@ -96,8 +89,19 @@ template <class T> void CircularBuffer<T>::Resize(size_t size) {
 
 template <class T> void CircularBuffer<T>::PushBack(const T &v) {
   T *e = End();
-  assert(e < m_buffer + m_buffer_size);
+  assert(e >= m_buffer);
+  assert(e < m_buffer + (m_capacity * 2));
   *e = v;
+  if(e < m_buffer + m_capacity) {
+    assert(e + m_capacity >= m_buffer);
+    assert(e + m_capacity < m_buffer + (m_capacity * 2));
+    *(e + m_capacity) = v;
+  } else {
+    assert(e - m_capacity >= m_buffer);
+    assert(e - m_capacity < m_buffer + (m_capacity * 2));
+    *(e - m_capacity) = v;
+  }
+
   PushBack((size_t)1);
 }
 
